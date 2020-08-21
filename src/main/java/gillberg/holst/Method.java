46e950d2d05d0672540f4a060f402bd3ab2285fc
Gustaf Holst <guho1700@student.miun.se>
2020-08-21 17:00:50 +0200

    refactor Method

--- src/main/java/gillberg/holst/Method.java
+++ src/main/java/gillberg/holst/Method.java
@@ -1,21 +1,29 @@
 package gillberg.holst;
 
 import com.github.javaparser.ast.body.MethodDeclaration;
+import gillberg.holst.enums.Feature;
+import gillberg.holst.enums.Paradigm;
+import gillberg.holst.enums.ValueNotSetException;
+import gillberg.holst.exceptions.FeatureAlreadySetException;
+import gillberg.holst.exceptions.FeatureNotSetException;
+import gillberg.holst.exceptions.UnknownParadigmException;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class Method {
+public class Method extends MethodDeclaration{
+
+    public ReadabilityComplexity originalReadabilityComplexity = new ReadabilityComplexity();
+    public ReadabilityComplexity refactoredReadabilityComplexity = new ReadabilityComplexity();
+
+    public CalculatedFeatures originalCalculatedFeatures = new CalculatedFeatures();
+    public CalculatedFeatures refactoredCalculatedFeatures = new CalculatedFeatures();
 
     public final static String NOT_SET = "not set";
 
     public final String className;
     public final String returnType;
     public final String signature;
-    private double rp_readability = Double.MIN_VALUE;
-    private int rp_complexity = Integer.MIN_VALUE;
-    private double oo_readability = Double.MIN_VALUE;
-    private int oo_complexity = Integer.MIN_VALUE;
 
     public int rp_num_stops = 0;
     public int oo_num_stops = 0;
@@ -50,14 +58,6 @@ public class Method {
     public int rp_num_and_or = 0;
     public int oo_num_and_or = 0;
 
-    public double oo_fk_reading_ease = 0.0;
-    public double oo_fk_grade_level = 0.0;
-    public double rp_fk_reading_ease = 0.0;
-    public double rp_fk_grade_level = 0.0;
-
-    public double oo_scalabrino = 0.0;
-    public double rp_scalabrino = 0.0;
-
     public Method(String className, MethodDeclaration declaration) {
         this.className = className;
 
@@ -74,87 +74,84 @@ public class Method {
         this.returnType = null;
     }
 
-    private boolean OOreadabilitySet() {
-        return Double.compare(this.oo_readability, Double.MIN_VALUE) != 0;
+    private boolean isOriginalValueMissing(Number[] values) {
+        return values[Paradigm.imperative.ordinal()] == null;
     }
 
-    private boolean RPreadabilitySet() {
-        return Double.compare(this.rp_readability, Double.MIN_VALUE) != 0;
+    private boolean isRefactoredValueMissing(Number[] values) {
+        return values[Paradigm.reactive.ordinal()] == null;
     }
 
-    private boolean OOcomplexitytySet() {
-        return Integer.compare(this.oo_complexity, Integer.MIN_VALUE) != 0;
+    private boolean isAnyValueMissing(Number[] values) {
+        return  isOriginalValueMissing(values) || isRefactoredValueMissing(values);
     }
 
-    private boolean RPcomplexitySet() {
-        return Integer.compare(this.rp_complexity, Integer.MIN_VALUE) != 0;
+    private Number[] tryToCreateNumberArrayWithValues(Number original, Number refactored) throws ValueNotSetException {
+        Number[] values = new Number[2];
+        values[Paradigm.imperative.ordinal()] = original;
+        values[Paradigm.reactive.ordinal()] = refactored;
+
+        if (isAnyValueMissing(values)) {
+            throw new ValueNotSetException();
+        }
+
+        return values;
     }
 
-    public String OOreadability() {
-        return OOreadabilitySet() ? String.valueOf(oo_readability) : NOT_SET;
+    public Number[] getBuseReadability() throws ValueNotSetException {
+        try {
+            Number[] readabilities = tryToCreateNumberArrayWithValues(
+                    originalReadabilityComplexity.buseReadability,
+                    refactoredReadabilityComplexity.buseReadability);
+
+            return readabilities;
+        } catch (ValueNotSetException vnse) {
+            throw new ValueNotSetException("Buse readability missing");
+        }
     }
 
-    public String RPreadability() {
-        return RPreadabilitySet() ? String.valueOf(rp_readability) : NOT_SET;
+    public Number[] getScalabrinoReadability() throws ValueNotSetException {
+        try {
+            Number[] readabilities = tryToCreateNumberArrayWithValues(
+                    originalReadabilityComplexity.scalabrinoReadability,
+                    refactoredReadabilityComplexity.scalabrinoReadability);
+
+            return readabilities;
+        } catch (ValueNotSetException vnse) {
+            throw new ValueNotSetException("Scalabrino readability missing");
+        }
     }
 
-    public String OOcomplexity() {
-        return OOcomplexitytySet() ? String.valueOf(oo_complexity) : NOT_SET;
+    public Number[] getCyclomaticComplexity() throws ValueNotSetException {
+        try {
+            Number[] complexities = tryToCreateNumberArrayWithValues(
+                    originalReadabilityComplexity.cyclomaticComplexity,
+                    refactoredReadabilityComplexity.cyclomaticComplexity);
+
+            return complexities;
+        } catch (ValueNotSetException vnse) {
+            throw new ValueNotSetException("Cyclomatic complexity missing");
+        }
     }
 
-    public String RPcomplexity() {
-        return RPcomplexitySet() ? String.valueOf(rp_complexity) : NOT_SET;
+    public Number[] getValuesForFeature(Feature feature) throws FeatureNotSetException, ValueNotSetException {
+        Number[] complexities = tryToCreateNumberArrayWithValues(
+                originalCalculatedFeatures.getCalculatedFeature(feature),
+                refactoredCalculatedFeatures.getCalculatedFeature(feature));
+
+        return complexities;
     }
 
-    public double getOo_readability() {
-        return oo_readability;
-    }
-
-    public void setOo_readability(double oo_readability) throws Exception {
-        if (OOreadabilitySet())
-            throw new Exception("OO readability is already set");
-        this.oo_readability = oo_readability;
-    }
-
-    public int getOo_complexity() {
-        return oo_complexity;
-    }
-
-    public void setOo_complexity(int oo_complexity) throws Exception {
-        if (this.oo_complexity != Integer.MIN_VALUE)
-            throw new Exception("OO complexity is already set");
-        this.oo_complexity = oo_complexity;
-    }
-
-    public double getRp_readability() {
-        return rp_readability;
-    }
-
-    public void setRp_readability(double rp_readability) throws Exception {
-        if (Double.compare(this.rp_readability, Double.MIN_VALUE) != 0)
-            throw new Exception("RP readability is already set");
-        this.rp_readability = rp_readability;
-    }
-
-    public int getRp_complexity() {
-        return rp_complexity;
-    }
-
-    public void setRp_complexity(int rp_complexity) throws Exception {
-        if (this.rp_complexity != Integer.MIN_VALUE)
-            throw new Exception("RP complexity is already set");
-        this.rp_complexity = rp_complexity;
-    }
-
-    public String toString() {
-        return String.format("Class: %s\t " +
-                "return type: %s\t " +
-                "name: %s\t" +
-                "RP_readability: %s\t" +
-                "RP_complexity: %s\t" +
-                "OO_readability %s\t" +
-                "OO_complexity: %s" +
-                "\n", className, returnType, signature, RPreadability(), RPcomplexity(), OOreadability(), OOcomplexity());
+    public void setValueForFeature(Feature feature, Paradigm p, Number value) throws FeatureAlreadySetException, UnknownParadigmException {
+        if (p == Paradigm.imperative) {
+            originalCalculatedFeatures.setCalculatedFeature(feature, value);
+        }
+        else if (p == Paradigm.reactive) {
+            refactoredCalculatedFeatures.setCalculatedFeature(feature, value);
+        }
+        else {
+            throw new UnknownParadigmException(feature.toString());
+        }
     }
 
     @Override
